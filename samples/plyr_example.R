b10341b1354d63bf844792f138b721f36ae037c5
JS <jiata@microsoft.com>
2017-05-22 17:04:38 -0700

    Update plyr_example.R

--- samples/plyr_example.R
+++ samples/plyr_example.R
@@ -1,17 +1,41 @@
-library(doAzureParallel)
-library(plyr)
+# =============
+# === Setup ===
+# =============
 
+# install packages from github
+library(devtools)
+install_github("azure/razurebatch", ref="release")
+install_github("azure/doazureparallel", ref="release")
+
+# import packages
+library(doAzureParallel)
+
+# create credentials config files
+generateCredentialsConfig("credentials.json")
+
+# set azure credentials
 setCredentials("credentials.json")
 
+# generate cluster config json file
+generateClusterConfig("cluster.json")
+
 # Creating an Azure parallel backend
-cluster <- makeCluster("cluster_settings.json")
+cluster <- makeCluster(clusterSetting = "cluster.json")
 
 # Register your Azure parallel backend to the foreach implementation
 registerDoAzureParallel(cluster)
+
+# ==========================================================
+# === Using plyr with doAzureParallel's parallel backend ===
+# ==========================================================
+
+# import plyr
+library(plyr)
 
 # For more information on plyr, https://github.com/hadley/plyr
 dlply(iris, .(Species), function(x)
   lm(x$Sepal.Width ~ x$Petal.Length, data=x),
   .parallel=TRUE, .paropts = list(.packages = NULL,.export="iris"))
 
+# de-provision your cluster in Azure
 stopCluster(cluster)
