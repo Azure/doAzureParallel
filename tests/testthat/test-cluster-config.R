5a23f22a9e6e788fbe7af3c3a0628398de6334d1
Brian <l.brian.hoang@gmail.com>
2017-06-23 10:43:22 -0700

    Added if checks for null tests and more validation tests

--- tests/testthat/test-cluster-config.R
+++ tests/testthat/test-cluster-config.R
@@ -11,6 +11,22 @@ test_that("validating a cluster config file with null pool property", {
   on.exit(file.remove(clusterConfig))
 })
 
+test_that("validating a cluster config file with bad autoscale formula property", {
+  clusterConfig <- "badcluster.json"
+
+  generateClusterConfig(clusterConfig)
+  config <- jsonlite::fromJSON(clusterConfig)
+  config$poolSize$autoscaleFormula <- "BAD_FORMULA"
+
+  configJson <- jsonlite::toJSON(config, auto_unbox = TRUE, pretty = TRUE)
+  write(configJson, file = paste0(getwd(), "/", clusterConfig))
+
+  expect_error(validateClusterConfig(clusterConfig))
+
+  on.exit(file.remove(clusterConfig))
+})
+
+
 test_that("validating a cluster config file with incorrect data types", {
   clusterConfig <- "badcluster.json"
 
