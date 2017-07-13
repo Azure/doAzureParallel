e96ab31584815705073eebb144e7745a73bb00aa
Brian Hoang <brhoan@microsoft.com>
2017-07-13 15:29:29 -0700

    Removed verbose message on command line

--- tests/testthat/test-command-line.R
+++ tests/testthat/test-command-line.R
@@ -9,6 +9,6 @@ test_that("single command on command line", {
 test_that("multiple commands on command line", {
   commands <- c("ls", "echo \"hello\"", "cp origfile newfile")
   commandLine <- linuxWrapCommands(commands)
-  cat(commandLine)
+
   expect_equal(commandLine, "/bin/bash -c \"set -e; set -o pipefail; ls; echo \"hello\"; cp origfile newfile; wait\"")
 })
