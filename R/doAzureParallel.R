10f91ae7bc21d0734f0b6653576924b189688974
Zhiqiang Feng <zfeng@microsoft.com>
2017-07-26 18:04:08 +0000

    fix subscription out of bounds error

--- R/doAzureParallel.R
+++ R/doAzureParallel.R
@@ -290,20 +290,22 @@ setVerbose <- function(value = FALSE){
     endIndices[length(startIndices)] <- ntasks
   }
 
-  tasks <- lapply(1:length(endIndices), function(i){
-    startIndex <- startIndices[i]
-    endIndex <- endIndices[i]
-    taskId <- paste0(id, "-task", i)
+  if(length(endIndices) > 0){
+    tasks <- lapply(1:length(endIndices), function(i){
+      startIndex <- startIndices[i]
+      endIndex <- endIndices[i]
+      taskId <- paste0(id, "-task", i)
 
-    .addTask(id,
-            taskId = taskId,
-            rCommand =  sprintf("Rscript --vanilla --verbose $AZ_BATCH_JOB_PREP_WORKING_DIR/worker.R %s %s %s %s > %s.txt", "$AZ_BATCH_JOB_PREP_WORKING_DIR", "$AZ_BATCH_TASK_WORKING_DIR", jobFileName, paste0(taskId, ".rds"), taskId),
-            args = argsList[startIndex:endIndex],
-            envir = .doAzureBatchGlobals,
-            packages = obj$packages)
+      .addTask(id,
+              taskId = taskId,
+              rCommand =  sprintf("Rscript --vanilla --verbose $AZ_BATCH_JOB_PREP_WORKING_DIR/worker.R %s %s %s %s > %s.txt", "$AZ_BATCH_JOB_PREP_WORKING_DIR", "$AZ_BATCH_TASK_WORKING_DIR", jobFileName, paste0(taskId, ".rds"), taskId),
+              args = argsList[startIndex:endIndex],
+              envir = .doAzureBatchGlobals,
+              packages = obj$packages)
 
-    return(taskId)
-  })
+      return(taskId)
+    })
+  }
 
   updateJob(id)
 
@@ -386,18 +388,20 @@ setVerbose <- function(value = FALSE){
   azureStorageUrl <- paste0("http://", storageCredentials$name,".blob.core.windows.net/", id)
 
   staticHtml <- "<h1>Errors:</h1>"
-  for(i in 1:length(failTasks)){
-    if(failTasks[i] == 1){
+  if(length(failTasks) > 0){
+    for(i in 1:length(failTasks)){
+      if(failTasks[i] == 1){
 
-      stdoutFile <- paste0(azureStorageUrl, "/stdout")
-      stderrFile <- paste0(azureStorageUrl, "/stderr")
-      RstderrFile <- paste0(azureStorageUrl, "/logs")
+        stdoutFile <- paste0(azureStorageUrl, "/stdout")
+        stderrFile <- paste0(azureStorageUrl, "/stderr")
+        RstderrFile <- paste0(azureStorageUrl, "/logs")
 
-      stdoutFile <- paste0(stdoutFile, "/", id, "-task", i, "-stdout.txt", queryParameterUrl)
-      stderrFile <- paste0(stderrFile, "/", id, "-task", i, "-stderr.txt", queryParameterUrl)
-      RstderrFile <- paste0(RstderrFile, "/", id, "-task", i, ".txt", queryParameterUrl)
+        stdoutFile <- paste0(stdoutFile, "/", id, "-task", i, "-stdout.txt", queryParameterUrl)
+        stderrFile <- paste0(stderrFile, "/", id, "-task", i, "-stderr.txt", queryParameterUrl)
+        RstderrFile <- paste0(RstderrFile, "/", id, "-task", i, ".txt", queryParameterUrl)
 
-      staticHtml <- paste0(staticHtml, 'Task ', i, ' | <a href="', stdoutFile,'">', "stdout.txt",'</a> |', ' <a href="', stderrFile,'">', "stderr.txt",'</a> | <a href="', RstderrFile,'">', "R output",'</a> <br/>')
+        staticHtml <- paste0(staticHtml, 'Task ', i, ' | <a href="', stdoutFile,'">', "stdout.txt",'</a> |', ' <a href="', stderrFile,'">', "stderr.txt",'</a> | <a href="', RstderrFile,'">', "R output",'</a> <br/>')
+      }
     }
   }
 
