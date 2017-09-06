a7e11b7f1b112233deda9f4b9158f9cce0e53773
Pablo <paselem@microsoft.com>
2017-09-06 09:17:12 -0700

    wrap bioconductor install command in Rscript

--- samples/package_management/bioc_setup.sh
+++ samples/package_management/bioc_setup.sh
@@ -13,4 +13,4 @@ fi
 echo "export PATH=/usr/lib64/microsoft-r/3.4/lib64/R/bin:$PATH" >> /etc/environment
 
 # Install bioconductor
-source("https://bioconductor.org/biocLite.R")
+Rscript -e 'source("https://bioconductor.org/biocLite.R")'
