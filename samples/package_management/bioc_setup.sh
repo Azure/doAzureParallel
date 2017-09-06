3056e460008cfec3e46d833c10b4ffa9f58f3a5c
Pablo <paselem@microsoft.com>
2017-09-06 09:11:04 -0700

    install bioconductor

--- samples/package_management/bioc_setup.sh
+++ samples/package_management/bioc_setup.sh
@@ -11,3 +11,6 @@ fi
 
 # Update PATH on the node permanently
 echo "export PATH=/usr/lib64/microsoft-r/3.4/lib64/R/bin:$PATH" >> /etc/environment
+
+# Install bioconductor
+source("https://bioconductor.org/biocLite.R")
