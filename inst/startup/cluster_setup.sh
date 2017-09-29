f29d49f8a086bcb999d5e5bda0e85d1f0ac34c68
Pablo <paselem@microsoft.com>
2017-09-29 13:10:01 -0700

    remove starting container from setup script

--- inst/startup/cluster_setup.sh
+++ inst/startup/cluster_setup.sh
@@ -6,7 +6,7 @@
 
 
 container_name=$1
-docker_run_cmd=$2
+# docker_run_cmd=$2
 
 apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 apt-get -y install apt-transport-https
@@ -34,16 +34,16 @@ if [ $? -ne 0 ]; then
 fi
 
 # If the container already exists just restart. Otherwise create it
-if [ "$(docker ps -a -q -f name=$container_name)" ]; then
-    echo "Docker container is already setup. Restarting it."
-    docker restart $container_name
-else
-    echo "Creating docker container."
-    # Start docker
-    eval $docker_run_cmd
-
-    # Setup symbolic link for the docker logs
-    docker_log=$(docker inspect --format='{{.LogPath}}' $container_name)
-    mkdir -p $AZ_BATCH_TASK_WORKING_DIR/logs
-    ln -s $docker_log $AZ_BATCH_TASK_WORKING_DIR/logs/docker.log
-fi
+# if [ "$(docker ps -a -q -f name=$container_name)" ]; then
+#     echo "Docker container is already setup. Restarting it."
+#     docker restart $container_name
+# else
+#     echo "Creating docker container."
+#     # Start docker
+#     eval $docker_run_cmd
+# 
+#     # Setup symbolic link for the docker logs
+#     docker_log=$(docker inspect --format='{{.LogPath}}' $container_name)
+#     mkdir -p $AZ_BATCH_TASK_WORKING_DIR/logs
+#     ln -s $docker_log $AZ_BATCH_TASK_WORKING_DIR/logs/docker.log
+# fi
