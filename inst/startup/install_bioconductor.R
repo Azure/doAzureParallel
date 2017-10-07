9eed803628884b8b8eefcb82ba66dbed04ed0fa1
Pablo <da_spaz@hotmail.com>
2017-10-06 20:39:47 -0700

    update bioconductor install for non-jobPrep installs

---
+++ inst/startup/install_bioconductor.R
@@ -0,0 +1,35 @@
+#!/usr/bin/Rscript
+args <- commandArgs(trailingOnly = TRUE)
+
+status <- tryCatch({
+  jobPrepDirectory <- Sys.getenv("AZ_BATCH_JOB_PREP_WORKING_DIR")
+  .libPaths(c("/mnt/batch/tasks/shared/R/packages", .libPaths()))
+  
+  if (jobPrepDirectory != "") {
+    .libPaths(c(jobPrepDirectory, .libPaths()))
+  }
+  
+  library(BiocInstaller)
+  for (package in args) {
+    if (!require(package, character.only = TRUE)) {
+      biocLite(pkgs = package)
+      require(package, character.only = TRUE)
+    }
+  }
+
+  0
+},
+error = function(e) {
+  cat(sprintf(
+    "Error getting parent environment: %s\n",
+    conditionMessage(e)
+  ))
+
+  # Install packages doesn't return a non-exit code.
+  # Using '1' as the default non-exit code
+  1
+})
+
+quit(save = "yes",
+     status = status,
+     runLast = FALSE)
