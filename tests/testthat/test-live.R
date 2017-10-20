4eb37737380e290e557f501bd31d8c3fe87e6651
Brian <l.brian.hoang@gmail.com>
2017-10-20 11:38:12 -0700

    Fix: Do not use task chunk size during cloud combine function (#152)
    * Fix task counts when group of tasks are not equal
    
    * Added chunksize test
    
    * Fixed pass case

--- tests/testthat/test-live.R
+++ tests/testthat/test-live.R
@@ -27,3 +27,32 @@ test_that("Scenario Test", {
   testthat::expect_equal(res,
                          list(2, 2, 2, 2))
 })
+
+test_that("Chunksize Test", {
+  testthat::skip_on_travis()
+  credentialsFileName <- "credentials.json"
+  clusterFileName <- "cluster.json"
+
+  doAzureParallel::generateCredentialsConfig(credentialsFileName)
+  doAzureParallel::generateClusterConfig(clusterFileName)
+
+  doAzureParallel::setCredentials(credentialsFileName)
+  cluster <- doAzureParallel::makeCluster(clusterFileName)
+  doAzureParallel::registerDoAzureParallel(cluster)
+
+  '%dopar%' <- foreach::'%dopar%'
+  res <-
+    foreach::foreach(i = 1:10, .options.azure = list(chunkSize = 3)) %dopar% {
+      i
+    }
+
+  doAzureParallel::stopCluster(cluster)
+
+  testthat::expect_equal(length(res),
+                         10)
+
+  for (i in 1:10) {
+    testthat::expect_equal(res[[i]],
+                           i)
+  }
+})
