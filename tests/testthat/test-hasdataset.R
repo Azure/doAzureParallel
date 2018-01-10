e857c9a6e75e189dce7f8a7e9b06e1d9c025c235
Brian <l.brian.hoang@gmail.com>
2018-01-09 16:31:22 -0800

    Removed before install, added github package of nycflights13

--- tests/testthat/test-hasdataset.R
+++ tests/testthat/test-hasdataset.R
@@ -1,21 +1,24 @@
-context("hasDataSet function")
+if (requireNamespace("nycflights13", quietly = TRUE)) {
+  context("hasDataSet function")
 
-test_that("Arguments contains data set", {
-  byCarrierList <- split(nycflights13::flights, nycflights13::flights$carrier)
-  it <- iterators::iter(byCarrierList)
-  argsList <- as.list(it)
+  test_that("Arguments contains data set", {
+    byCarrierList <- split(nycflights13::flights, nycflights13::flights$carrier)
+    it <- iterators::iter(byCarrierList)
+    argsList <- as.list(it)
 
-  hasDataSet <- hasDataSet(argsList)
+    hasDataSet <- hasDataSet(argsList)
 
-  expect_equal(hasDataSet, TRUE)
-})
+    expect_equal(hasDataSet, TRUE)
+  })
 
-test_that("Arguments does not contain data set", {
-  args <- seq(1:10)
-  it <- iterators::iter(args)
-  argsList <- as.list(it)
+  test_that("Arguments does not contain data set", {
+    args <- seq(1:10)
+    it <- iterators::iter(args)
+    argsList <- as.list(it)
 
-  hasDataSet <- hasDataSet(argsList)
+    hasDataSet <- hasDataSet(argsList)
 
-  expect_equal(hasDataSet, FALSE)
-})
+    expect_equal(hasDataSet, FALSE)
+  })
+
+}
