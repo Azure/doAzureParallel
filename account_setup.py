187a6095d16d7196bdd3f7c351f5d33b467d3033
zfengms <zfeng@microsoft.com>
2018-04-25 18:45:09 -0700

    storage key

--- account_setup.py
+++ account_setup.py
@@ -114,7 +114,7 @@ def storage_account_get_endpoint_suffix(credentials, subscription_id, **kwargs):
         :param **region: str
     """
     storage_management_client = StorageManagementClient(credentials, subscription_id)
-    storage_account = storage_management_client.storage_accounts.get(
+    storage_account = storage_management_client.storage_accounts.get_properties(
         resource_group_name=kwargs.get("resource_group", DefaultSettings.resource_group),
         account_name=kwargs.get("storage_account", DefaultSettings.storage_account)
     )
