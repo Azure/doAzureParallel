25f7c0af46ab3efea8d581c126cc5d8eca6ccd56
Brian <l.brian.hoang@gmail.com>
2018-05-10 15:54:27 -0700

    Added custom package script

---
+++ inst/startup/install_custom.R
@@ -0,0 +1,10 @@
+args <- commandArgs(trailingOnly = TRUE)
+
+packages <- list.files(args[1])
+for (i in 1:length(packages)) {
+  print(packages[i])
+  install.packages(packages[i],
+                   lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
+                                "R/packages"),
+                   type = "source")
+}
