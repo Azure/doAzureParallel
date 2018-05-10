ac9d4a64a46db7782d117affb6bd8e7b2065f24f
Brian <l.brian.hoang@gmail.com>
2018-05-10 16:41:34 -0700

    Add dependencies and fix pattern

--- inst/startup/install_custom.R
+++ inst/startup/install_custom.R
@@ -1,10 +1,20 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-packages <- list.files(args[1], full.names = TRUE)
+if (length(args) > 2) {
+  if (is.null(args[2])) {
+    pattern = NULL
+  }
+  else {
+    pattern = args[2]
+  }
+}
+
+packages <- list.files(args[1], full.names = TRUE, pattern = pattern)
 for (i in 1:length(packages)) {
   print(packages[i])
   install.packages(packages[i],
                    lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
                                 "/R/packages"),
+                   dependencies = TRUE,
                    type = "source")
 }
