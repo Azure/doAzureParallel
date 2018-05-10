8c0232897e6bd989dcde71d673a4fa750a1cd29e
Brian <l.brian.hoang@gmail.com>
2018-05-10 16:07:17 -0700

    Fixed directory for installation

--- inst/startup/install_custom.R
+++ inst/startup/install_custom.R
@@ -5,6 +5,6 @@ for (i in 1:length(packages)) {
   print(packages[i])
   install.packages(packages[i],
                    lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
-                                "R/packages"),
+                                "/R/packages"),
                    type = "source")
 }
