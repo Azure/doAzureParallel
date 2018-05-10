75081f59bdc05fad502a5a3ae2d1d77e65356e4a
Brian <l.brian.hoang@gmail.com>
2018-05-10 16:22:08 -0700

    Fixed full folder directory

--- inst/startup/install_custom.R
+++ inst/startup/install_custom.R
@@ -1,6 +1,6 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-packages <- list.files(args[1])
+packages <- list.files(args[1], full.names = TRUE)
 for (i in 1:length(packages)) {
   print(packages[i])
   install.packages(packages[i],
