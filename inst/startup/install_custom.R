f7b202691f68e7d12bf84d3bf525f1f7e74e0297
Brian <l.brian.hoang@gmail.com>
2018-05-10 17:17:19 -0700

    Switching to devtools

--- inst/startup/install_custom.R
+++ inst/startup/install_custom.R
@@ -7,13 +7,21 @@ if (length(args) > 1) {
   }
 }
 
+devtoolsPackage <- "devtools"
+if (!require(devtoolsPackage, character.only = TRUE)) {
+  install.packages(devtoolsPackage)
+  require(devtoolsPackage, character.only = TRUE)
+}
+
 packages <- list.files(args[1], full.names = TRUE, pattern = pattern)
 for (i in 1:length(packages)) {
-  print(packages[i])
-  install.packages(packages[i],
-                   lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
-                                "/R/packages"),
-                   dependencies = TRUE,
-                   repos = "https://cloud.r-project.org",
-                   type = "source")
+  devtools::install(packages[i],
+                    lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
+                                 "/R/packages"))
+  # install.packages(packages[i],
+  #                  lib = paste0(Sys.getenv("AZ_BATCH_NODE_SHARED_DIR"),
+  #                               "/R/packages"),
+  #                  dependencies = TRUE,
+  #                  repos = "https://cloud.r-project.org",
+  #                  type = "source")
 }
