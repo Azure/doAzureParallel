a62cb93fb7b17791997e41905890da23dc755240
Brian <l.brian.hoang@gmail.com>
2018-05-15 12:17:01 -0700

    Clean up rscript

--- tests/test_scripts/test.sh
+++ tests/test_scripts/test.sh
@@ -5,11 +5,13 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 
 sudo apt-get update
-sudo apt-get install -y r-base r-base-dev r-cran-xml r-cran-rjava libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev
-sudo apt-get install -y xml2 default-jre default-jdk mesa-common-dev libglu1-mesa-dev freeglut3-dev
-sudo apt-get install -y mesa-common-dev libx11-dev r-cran-rgl r-cran-rglpk r-cran-rsymphony r-cran-plyr
-sudo apt-get install -y  r-cran-reshape  r-cran-reshape2 r-cran-rmysql
+sudo apt-get install -y r-base r-base-dev r-cran-xml libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev xml2
 
-Rscript -e "getwd(); install.packages('devtools'); devtools::install_deps(dependencies = TRUE);"
-Rscript -e "devtools::build(); devtools::test();"
+Rscript
+  -e "getwd();"
+  -e "install.packages('devtools');"
+  -e "devtools::install();"
+  -e "devtools::build();"
+  -e "devtools::test();"
+
