d53a9bd5ad9c15b9c935f80d809abde9f2da8d98
Brian <l.brian.hoang@gmail.com>
2018-05-15 12:30:53 -0700

    Added line breaks

--- tests/test_scripts/test.sh
+++ tests/test_scripts/test.sh
@@ -6,12 +6,12 @@ gpg -a --export E084DAB9 | sudo apt-key add -
 
 sudo apt-get update
 sudo apt-get install -y r-base r-base-dev r-cran-xml libcurl4-openssl-dev
-sudo apt-get install -y libssl-dev libxml2-dev openjdk-7-* libgdal-dev libproj-dev libgsl-dev xml2
+sudo apt-get install -y libssl-dev libxml2-dev libgdal-dev libproj-dev libgsl-dev xml2
 
-Rscript
-  -e "getwd();"
-  -e "install.packages('devtools');"
-  -e "devtools::install();"
-  -e "devtools::build();"
+Rscript \
+  -e "getwd();" \
+  -e "install.packages('devtools');" \
+  -e "devtools::install();" \
+  -e "devtools::build();" \
   -e "devtools::test();"
 
