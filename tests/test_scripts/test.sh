4434fc25dabbcf47ea1218ed008acf229f57ed62
Brian <l.brian.hoang@gmail.com>
2018-05-15 16:06:45 -0700

    Added environment variables to test

--- tests/test_scripts/test.sh
+++ tests/test_scripts/test.sh
@@ -9,6 +9,11 @@ sudo apt-get install -y r-base r-base-dev libcurl4-openssl-dev
 sudo apt-get install -y libssl-dev libxml2-dev libgdal-dev libproj-dev libgsl-dev
 
 sudo R \
+  -e "Sys.setenv(BATCH_ACCOUNT_NAME = '$BATCH_ACCOUNT_NAME')" \
+  -e "Sys.setenv(BATCH_ACCOUNT_KEY = '$BATCH_ACCOUNT_KEY')" \
+  -e "Sys.setenv(BATCH_ACCOUNT_URL = '$BATCH_ACCOUNT_URL')" \
+  -e "Sys.setenv(STORAGE_ACCOUNT_NAME = '$STORAGE_ACCOUNT_NAME')" \
+  -e "Sys.setenv(STORAGE_ACCOUNT_KEY = '$STORAGE_ACCOUNT_KEY')" \
   -e "getwd();" \
   -e "install.packages(c('devtools', 'remotes', 'testthat', 'roxygen2'));" \
   -e "devtools::install();" \
