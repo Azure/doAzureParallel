dee4d3660cc6ffe4bb28ade8230391b61c2dffb3
Brian <l.brian.hoang@gmail.com>
2018-05-15 14:41:46 -0700

    Test live

--- tests/testthat/test-live.R
+++ tests/testthat/test-live.R
@@ -27,6 +27,76 @@ test_that("Basic scenario test", {
   testthat::expect_equal(res, list(2, 2, 2, 2))
 })
 
+# Run this test for users to make sure the core features
+# of doAzureParallel are still working
+context("live scenario test")
+test_that("Basic Test 2", {
+  testthat::skip_on_travis()
+  credentialsFileName <- "credentials.json"
+  clusterFileName <- "cluster.json"
+
+  credentials <- list(
+    "sharedKey" = list(
+      "batchAccount" = list(
+        "name" = Sys.getenv("BATCH_ACCOUNT_NAME"),
+        "key" = Sys.getenv("BATCH_ACCOUNT_KEY"),
+        "url" = Sys.getenv("BATCH_ACCOUNT_URL")
+      ),
+      "storageAccount" = list(
+        "name" = Sys.getenv("STORAGE_ACCOUNT_NAME"),
+        "key" = Sys.getenv("STORAGE_ACCOUNT_KEY"),
+        "endpointSuffix" = "core.windows.net"
+      )
+    ),
+    "githubAuthenticationToken" = "",
+    "dockerAuthentication" = list("username" = "",
+                                  "password" = "",
+                                  "registry" = "")
+  )
+
+  # set your credentials
+  doAzureParallel::setCredentials(credentials)
+
+  clusterConfig <- list(
+    "name" = "test-pool",
+    "vmSize" = "Standard_D2_v2",
+    "maxTasksPerNode" = 1,
+    "poolSize" = list(
+      "dedicatedNodes" = list(
+        "min" = 0,
+        "max" = 0
+      ),
+      "lowPriorityNodes" = list(
+        "min" = 1,
+        "max" = 1
+      ),
+      "autoscaleFormula" = "QUEUE"
+    ),
+    "containerImage" = "rocker/tidyverse:latest",
+    "rPackages" = list(
+      "cran" = list(),
+      "github" = list(),
+      "bioconductor" = list()
+    ),
+    "commandLine" = list()
+  )
+
+  cluster <- doAzureParallel::makeCluster(clusterConfig)
+  doAzureParallel::registerDoAzureParallel(cluster)
+
+  '%dopar%' <- foreach::'%dopar%'
+  res <-
+    foreach::foreach(i = 1:4) %dopar% {
+      i
+    }
+
+  res
+
+  testthat::expect_equal(length(res), 4)
+  testthat::expect_equal(res, list(2, 2, 2, 2))
+})
+
+
 test_that("Chunksize Test", {
   testthat::skip("Live test")
   testthat::skip_on_travis()
