df248a55e65912d28e9f3caa019755262f0adc03
Brian <l.brian.hoang@gmail.com>
2019-02-10 00:30:32 -0800

    Added RProfile site

--- tests/test_scripts/unit_tests.sh
+++ tests/test_scripts/unit_tests.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 sudo R \
-  -e "Sys.setenv(BATCH_ACCOUNT_NAME = '$BATCH_ACCOUNT_NAME')" \
-  -e "Sys.setenv(BATCH_ACCOUNT_KEY = '$BATCH_ACCOUNT_KEY')" \
-  -e "Sys.setenv(BATCH_ACCOUNT_URL = '$BATCH_ACCOUNT_URL')" \
-  -e "Sys.setenv(STORAGE_ACCOUNT_NAME = '$STORAGE_ACCOUNT_NAME')" \
-  -e "Sys.setenv(STORAGE_ACCOUNT_KEY = '$STORAGE_ACCOUNT_KEY')" \
   -e "getwd();" \
   -e "devtools::install();" \
   -e "devtools::build();" \
