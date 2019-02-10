7510172a7421a9a58e0ef176022e824257e5697a
Brian <l.brian.hoang@gmail.com>
2019-02-09 17:20:52 -0800

    Passing in args to script

--- tests/test_scripts/unit_tests.sh
+++ tests/test_scripts/unit_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+BATCH_ACCOUNT_NAME = $1
+BATCH_ACCOUNT_KEY = $2
+BATCH_ACCOUNT_URL = $3
+STORAGE_ACCOUNT_NAME = $4
+STORAGE_ACCOUNT_KEY = $5
+
+echo "Start Unit Tests:"
+echo "Batch Account: $(BATCH_ACCOUNT_NAME)"
+echo "Storage Account: $(STORAGE_ACCOUNT_NAME)"
 
 sudo R \
   -e "Sys.setenv(BATCH_ACCOUNT_NAME = '$BATCH_ACCOUNT_NAME')" \
@@ -13,3 +22,5 @@ sudo R \
   -e "res <- devtools::test(reporter='summary');" \
   -e "df <- as.data.frame(res);" \
   -e "if(sum(df[['failed']]) > 0 || any(df[['error']])) { q(status=1) }"
+
+echo "Unit Test Completed."
