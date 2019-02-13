5ddfbaec31fb0c3ab74547a22802dddfb4bcb96b
Brian <l.brian.hoang@gmail.com>
2019-02-13 10:34:14 -0800

    Fixed directory call for testthat

--- tests/test_scripts/unit_tests.sh
+++ tests/test_scripts/unit_tests.sh
@@ -5,6 +5,6 @@ sudo R \
   -e "devtools::install();" \
   -e "devtools::build();" \
   -e "devtools::load_all();" \
-  -e "res <- testthat::test_dir('tests/testthat/unit_tests', reporter='summary');" \
+  -e "res <- testthat::test_dir('../testthat/unit_tests', reporter='summary');" \
   -e "df <- as.data.frame(res);" \
   -e "if(sum(df[['failed']]) > 0 || any(df[['error']])) { q(status=1) }"
