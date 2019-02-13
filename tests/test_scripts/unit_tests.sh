8713a5213c3c4fdef0f1d2d5ef63f6de6a5f83b8
Brian <l.brian.hoang@gmail.com>
2019-02-13 10:01:21 -0800

    Removed other tests

--- tests/test_scripts/unit_tests.sh
+++ tests/test_scripts/unit_tests.sh
@@ -8,9 +8,3 @@ sudo R \
   -e "res <- testthat::test_dir('tests/testthat/unit_tests', reporter='summary');" \
   -e "df <- as.data.frame(res);" \
   -e "if(sum(df[['failed']]) > 0 || any(df[['error']])) { q(status=1) }"
-  -e "res <- testthat::test_dir('tests/testthat/core', reporter='summary');" \
-  -e "df <- as.data.frame(res);" \
-  -e "if(sum(df[['failed']]) > 0 || any(df[['error']])) { q(status=1) }"
-  -e "res <- testthat::test_dir('tests/testthat/integration_tests', reporter='summary');" \
-  -e "df <- as.data.frame(res);" \
-  -e "if(sum(df[['failed']]) > 0 || any(df[['error']])) { q(status=1) }"
